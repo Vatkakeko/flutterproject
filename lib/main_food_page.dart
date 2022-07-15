4f3b61e070589cc4576e08d08d2e6ffdf19b29a9
kanhchna <kanhna.roth@student.cadt.edu.kh>
2022-07-16 01:03:52 +0700

    continue project time 1:23:56

--- lib/main_food_page.dart
+++ lib/main_food_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/home/foot_page_body.dart';
+import 'package:food_delivery_app/utils/colors.dart';
+import 'package:food_delivery_app/widgets/big_text.dart';
+import 'package:food_delivery_app/widgets/small_text.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({Key? key}) : super(key: key);
@@ -16,28 +20,41 @@ class _MainFoodPage extends State<MainFoodPage> {
       children: [
         Container(
           child: Container(
+            margin: EdgeInsets.only(top: 45,bottom: 15),
+            padding: EdgeInsets.only(left: 20, right: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
                   children: [
-                    Text("Country"),
-                    Text("City"),
+                    BigText(text: "Phnom Penh", color: AppColors.mainColor,),
+                    Row(
+                      children: [
+                        SmallText(text: "Nasingdi", color: Colors.black54,),
+                        Icon(Icons.arrow_drop_down)
+                      ],
+                    )
                   ],
                 ),
-                Container(
+                Center(
+                  child:Container(
                   width: 45,
                   height: 45,
+                  child: Icon(Icons.search, color: Colors.white,),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
-                    color: Colors.blue,
+                    color: AppColors.mainColor,
                   ),
                 ),
+                )
+                
               ],
             ),
           ),
         ),
+        FoodPageBody(),
       ],
     ));
+
   }
 }
